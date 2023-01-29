7034817609ca326f57b4e19ddd4b5c8d1c398077
aimeric44uwu <114582283+aimeric44uwu@users.noreply.github.com>
2023-01-29 19:45:49 +0100

    Add files via upload

---
+++ router/logout.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const checkAuthenticated = require("../auth/CheckAuth")
+
+router.get('/',checkAuthenticated, (req, res, next) => {
+	if (req.session) {
+		// delete session object
+		req.session.destroy((err) => {
+			if (err) {
+				return next(err);
+			} else {
+
+				return res.redirect('/');
+			}
+		});
+	}
+});
+
+
+
+module.exports = router;
