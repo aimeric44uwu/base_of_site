49d506fb99cd040627fcbed9218570aba2f66188
aimeric44uwu <114582283+aimeric44uwu@users.noreply.github.com>
2023-01-29 19:46:57 +0100

    Add files via upload

---
+++ models/cart.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    products: [
+      {
+        productId: Number,
+        quantity: Number,
+      }
+    ],
+    active: {
+      type: Boolean,
+      default: true
+    },
+    modifiedOn: {
+      type: Date,
+      default: Date.now
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Cart", CartSchema);
